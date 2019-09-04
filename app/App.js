d2ab5d7af36cac7634751f8f60d5fcbd7692d16f
unixcornthreads <doniawamer@gmail.com>
2019-09-04 14:46:15 -0400

    senior login workin yay

--- app/App.js
+++ app/App.js
@@ -1,122 +1,161 @@
 import React from "react";
-import { StyleSheet, Text, View,
+import {
+  StyleSheet,
+  Text,
+  View,
   Image,
   Platform,
   ScrollView,
-  TouchableOpacity,
-   } from "react-native";
+  TextInput,
+} from "react-native";
 
 import {
   createAppContainer,
   StackActions,
   NavigationActions
 } from "react-navigation";
+
 import { createStackNavigator } from "react-navigation-stack";
 import Button from "react-native-button";
+import Inputs from './inputs.js'
+
 
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}
-      >
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require("../app/assets/Oldlogo.png")
-                : require("../app/assets/Oldlogo.png")
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-
-        <View style={styles.titleContainer}>
-          <Text style={styles.getStartedText}>Request Help.  </Text>
-          <Text style={styles.titleLettersblue}>Give</Text>
-          <Text style={styles.titleLettersgreen}> Back.</Text>
-        </View>
-        <Text style={styles.titleLetters}>Fostering a sense of community.</Text>
-
-        <View style={{ alignItems: "center" }}>
-          <Image
-            source={require("../app/assets/pedestriancrossing.png")}
-            style={styles.backgroundContainer}
-          />
-        </View>
-
-        <Button
-          containerStyle={{
-            padding: 10,
-            height: 45,
-            width: 300,
-            marginHorizontal: 65,
-            overflow: "hidden",
-            borderRadius: 10,
-            backgroundColor: "#BFB093"
-          }}
-          disabledContainerStyle={{ backgroundColor: "grey" }}
-          style={{ fontSize: 20, color: "white" }}
-          onPress={() => this.props.navigation.navigate('SLogin')}
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
         >
-          Senior Login
-        </Button>
+          <View style={styles.welcomeContainer}>
+            <Image
+              source={
+                __DEV__
+                  ? require("../app/assets/Oldlogo.png")
+                  : require("../app/assets/Oldlogo.png")
+              }
+              style={styles.welcomeImage}
+            />
+          </View>
 
-        <View style={styles.titleContainer}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.getStartedText}>Request Help. </Text>
+            <Text style={styles.titleLettersblue}>Give</Text>
+            <Text style={styles.titleLettersgreen}> Back.</Text>
+          </View>
+          <Text style={styles.titleLetters}>
+            Fostering a sense of community.
+          </Text>
 
-        <Button
-          containerStyle={{
-            padding: 10,
-            height: 45,
-            width: 300,
-            marginHorizontal: -25,
-            overflow: "hidden",
-            borderRadius: 10,
-            backgroundColor: "#BFB093"
-          }}
-          disabledContainerStyle={{ backgroundColor: "grey" }}
-          style={{ fontSize: 20, color: "white" }}
-          onPress={() => this.props.navigation.navigate('VLogin')}
-        >
-          Volunteer Log In
-        </Button>
-        </View>
+          <View style={{ alignItems: "center" }}>
+            <Image
+              source={require("../app/assets/pedestriancrossing.png")}
+              style={styles.backgroundContainer}
+            />
+          </View>
 
-        <Button containerStyle={{paddingTop: 20}} style={{ fontSize: 10, color: "#6F8D71"}}>
-          Terms of Service
-        </Button>
+          <Button
+            containerStyle={{
+              padding: 10,
+              height: 45,
+              width: 300,
+              marginHorizontal: 65,
+              overflow: "hidden",
+              borderRadius: 10,
+              backgroundColor: "#CDAD70"
+            }}
+            disabledContainerStyle={{ backgroundColor: "grey" }}
+            style={{ fontSize: 20, color: "white" }}
+            onPress={() => this.props.navigation.navigate("SLogin")}
+          >
+            Senior Login
+          </Button>
 
-      </ScrollView>
-    </View>
-    );
-  }
-}
+          <View style={styles.titleContainer}>
+            <Button
+              containerStyle={{
+                padding: 10,
+                height: 45,
+                width: 300,
+                marginHorizontal: -25,
+                overflow: "hidden",
+                borderRadius: 10,
+                backgroundColor: "#CDAD70"
+              }}
+              disabledContainerStyle={{ backgroundColor: "grey" }}
+              style={{ fontSize: 20, color: "white" }}
+              onPress={() => this.props.navigation.navigate("VLogin")}
+            >
+              Volunteer Login
+            </Button>
+          </View>
 
-// Seniors 
-class SeniorLogin extends React.Component {
-  render() {
-    return (
-      
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Senior Login</Text>
-
-        <Button
-          title="Login"
-          onPress={() => this.props.navigation.navigate('SDash')}
-        />
-
-        <Button
-          title="Go Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-
+          <Button
+            containerStyle={{ paddingTop: 20 }}
+            style={{ fontSize: 10, color: "#6F8D71" }}
+          >
+            Terms of Service
+          </Button>
+        </ScrollView>
       </View>
     );
   }
 }
+
+// Seniors
+class SeniorLogin extends React.Component {
+  render() {
+    return (
+      <ScrollView style={loginstyles.container}>
+        <View style={loginstyles.titleContainer}>
+          <Text style={loginstyles.titleLetters}>Login</Text>
+        </View>
+
+        <View style={loginstyles.contentContainer}>
+        <Inputs></Inputs>
+          
+        </View>
+
+        <Button
+          containerStyle={{
+            padding: 10,
+            height: 45,
+            width: 300,
+            overflow: "hidden",
+            marginTop: 15,
+            borderRadius: 10,
+            alignSelf: "center",
+            backgroundColor: "#6F8D71"
+          }}
+          disabledContainerStyle={{ backgroundColor: "grey" }}
+          style={{ fontSize: 16, color: "#FFF" }}
+          onPress = {() => this.props.navigation.navigate('SDash')}
+        >
+          Login
+        </Button>
+
+        <Text style={styles.passtext}>Forgot your password?</Text>
+      </ScrollView>
+
+      // <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+
+      //   <Button
+      //     title="Login"
+      //     onPress={() => this.props.navigation.navigate('SDash')}
+      //   />
+
+      //   <Button
+      //     title="Go Home"
+      //     onPress={() => this.props.navigation.navigate('Home')}
+      //   />
+
+      // </View>
+    );
+  }
+}
+
 
 class SeniorDash extends React.Component {
   render() {
@@ -126,19 +165,18 @@ class SeniorDash extends React.Component {
 
         <Button
           title="Favs"
-          onPress={() => this.props.navigation.navigate('SFav')}
+          onPress={() => this.props.navigation.navigate("SFav")}
         />
 
         <Button
           title="Profile"
-          onPress={() => this.props.navigation.navigate('SProfile')}
+          onPress={() => this.props.navigation.navigate("SProfile")}
         />
 
         <Button
           title="Logout"
-          onPress={() => this.props.navigation.navigate('Home')}
+          onPress={() => this.props.navigation.navigate("Home")}
         />
-
       </View>
     );
   }
@@ -152,19 +190,18 @@ class SeniorFav extends React.Component {
 
         <Button
           title="Dash"
-          onPress={() => this.props.navigation.navigate('SDash')}
+          onPress={() => this.props.navigation.navigate("SDash")}
         />
 
         <Button
           title="Profile"
-          onPress={() => this.props.navigation.navigate('SProfile')}
+          onPress={() => this.props.navigation.navigate("SProfile")}
         />
 
         <Button
           title="Logout"
-          onPress={() => this.props.navigation.navigate('Home')}
+          onPress={() => this.props.navigation.navigate("Home")}
         />
-
       </View>
     );
   }
@@ -178,19 +215,18 @@ class SeniorProfile extends React.Component {
 
         <Button
           title="Dash"
-          onPress={() => this.props.navigation.navigate('SDash')}
+          onPress={() => this.props.navigation.navigate("SDash")}
         />
 
         <Button
           title="Fav"
-          onPress={() => this.props.navigation.navigate('SFav')}
+          onPress={() => this.props.navigation.navigate("SFav")}
         />
 
         <Button
           title="Logout"
-          onPress={() => this.props.navigation.navigate('Home')}
+          onPress={() => this.props.navigation.navigate("Home")}
         />
-
       </View>
     );
   }
@@ -205,19 +241,17 @@ class VolunteerLogin extends React.Component {
 
         <Button
           title="Dashboard"
-          onPress={() => this.props.navigation.navigate('VDash')}
+          onPress={() => this.props.navigation.navigate("VDash")}
         />
 
         <Button
           title="Go Home"
-          onPress={() => this.props.navigation.navigate('Home')}
+          onPress={() => this.props.navigation.navigate("Home")}
         />
-
       </View>
     );
   }
 }
-
 
 class VolunteerDash extends React.Component {
   render() {
@@ -227,19 +261,18 @@ class VolunteerDash extends React.Component {
 
         <Button
           title="Tasks"
-          onPress={() => this.props.navigation.navigate('VTasks')}
+          onPress={() => this.props.navigation.navigate("VTasks")}
         />
 
         <Button
           title="Profile"
-          onPress={() => this.props.navigation.navigate('VProfile')}
+          onPress={() => this.props.navigation.navigate("VProfile")}
         />
 
         <Button
           title="Logout"
-          onPress={() => this.props.navigation.navigate('Home')}
+          onPress={() => this.props.navigation.navigate("Home")}
         />
-
       </View>
     );
   }
@@ -253,19 +286,18 @@ class VolunteerTasks extends React.Component {
 
         <Button
           title="Dash"
-          onPress={() => this.props.navigation.navigate('VDash')}
+          onPress={() => this.props.navigation.navigate("VDash")}
         />
 
         <Button
           title="Profile"
-          onPress={() => this.props.navigation.navigate('VProfile')}
+          onPress={() => this.props.navigation.navigate("VProfile")}
         />
 
         <Button
           title="Logout"
-          onPress={() => this.props.navigation.navigate('Home')}
+          onPress={() => this.props.navigation.navigate("Home")}
         />
-
       </View>
     );
   }
@@ -279,37 +311,35 @@ class VolunteerProfile extends React.Component {
 
         <Button
           title="Dash"
-          onPress={() => this.props.navigation.navigate('VDash')}
+          onPress={() => this.props.navigation.navigate("VDash")}
         />
 
         <Button
           title="Tasks"
-          onPress={() => this.props.navigation.navigate('VTasks')}
+          onPress={() => this.props.navigation.navigate("VTasks")}
         />
 
         <Button
           title="Logout"
-          onPress={() => this.props.navigation.navigate('Home')}
+          onPress={() => this.props.navigation.navigate("Home")}
         />
-
       </View>
     );
   }
 }
-
 
 // Routes
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
     SLogin: SeniorLogin,
-    VLogin: VolunteerLogin, 
+    VLogin: VolunteerLogin,
     SDash: SeniorDash,
     SFav: SeniorFav,
     SProfile: SeniorProfile,
     VDash: VolunteerDash,
     VTasks: VolunteerTasks,
-    VProfile: VolunteerProfile,
+    VProfile: VolunteerProfile
   },
   {
     initialRouteName: "Home"
@@ -320,16 +350,16 @@ const AppContainer = createAppContainer(RootStack);
 
 export default class App extends React.Component {
   render() {
-    return (
-     <AppContainer />
-    );
+    return <AppContainer />;
   }
 }
 
+// Styling 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: "#fff",
+    paddingTop: 15
   },
   developmentModeText: {
     marginBottom: 20,
@@ -339,7 +369,7 @@ const styles = StyleSheet.create({
     textAlign: "center"
   },
   contentContainer: {
-    paddingTop: 30
+    paddingTop: 10
   },
   welcomeContainer: {
     alignItems: "center",
@@ -356,14 +386,15 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     resizeMode: "contain",
-    marginTop: 3,
+    marginTop: 0,
     marginLeft: -10
   },
   welcomeImage: {
-    width: 100,
-    height: 100,
+    width: 110,
+    height: 110,
     resizeMode: "contain",
-    marginTop: 3,
+    marginTop: 0,
+    marginBottom: 3,
     marginLeft: -10
   },
   getStartedContainer: {
@@ -386,6 +417,14 @@ const styles = StyleSheet.create({
     color: "rgba(96,100,109, 1)",
     lineHeight: 24,
     textAlign: "center"
+  },
+  heading1: {
+    padding: 0,
+    margin: 0,
+    fontSize: 24,
+    color: "rgba(96,100,109, 1)",
+    lineHeight: 24,
+    textAlign: "left"
   },
 
   titleLetters: {
@@ -445,5 +484,70 @@ const styles = StyleSheet.create({
   helpLinkText: {
     fontSize: 14,
     color: "#2e78b7"
+  },
+  passtext: {
+    alignSelf: "center",
+    color: "#CDAD70",
+    paddingTop: 5,
+    textDecorationLine: "underline"
+  },
+  textHeading: {
+    marginBottom: 20,
+    padding: 20,
+    color: "#000",
+    fontSize: 25,
+    lineHeight: 19
+  },
+  textParagraph: {
+    marginBottom: 20,
+    color: "rgba(0,0,0,0.4)",
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: "center"
   }
 });
+
+const loginstyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 40
+  },
+  contentContainer: {
+    paddingTop: 40
+  },
+  titleContainer: {
+    alignItems: "center",
+    marginTop: 10,
+    marginHorizontal: 30,
+    flexDirection: "row",
+    marginBottom: 0,
+  },
+  backgroundContainer: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+    marginTop: 0,
+    marginLeft: -10
+  },
+  titleLetters: {
+    color: "rgba(96,100,109, 1)",
+    fontSize: 24,
+    lineHeight: 24,
+    alignSelf: "flex-start",
+    marginBottom: 0,
+  },
+  titleLettersblue: {
+    color: "#77A8AC",
+    fontSize: 20,
+    lineHeight: 24,
+    textAlign: "center"
+  },
+  titleLettersgreen: {
+    color: "#6F8D71",
+    fontSize: 20,
+    lineHeight: 24,
+    textAlign: "center"
+  }
+});
+
---
+++ app/inputs.js
@@ -0,0 +1,133 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet
+} from "react-native";
+
+class Inputs extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+  handleEmail = text => {
+    this.setState({ email: text });
+  };
+  handlePassword = text => {
+    this.setState({ password: text });
+  };
+  login = (email, pass) => {
+    alert("email: " + email + " password: " + pass);
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={loginstyles.textLetters}>Email</Text>
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder=" Enter email here"
+          placeholderTextColor="#77A8AC"
+          autoCapitalize="none"
+        />
+
+        <Text style={loginstyles.pass}>Password</Text>
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder=" Enter Password here"
+          placeholderTextColor="#77A8AC"
+          autoCapitalize="none"
+        />
+      </View>
+    );
+  }
+}
+export default Inputs;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20
+  },
+  input: {
+    margin: 15,
+    height: 40,
+    borderColor: "#77A8AC",
+    borderWidth: 1,
+    padding: 5,
+    marginHorizontal: 30,
+    width: 350,
+  },
+  submitButton: {
+    backgroundColor: "#77A8AC",
+    padding: 10,
+    margin: 15,
+    height: 40
+  },
+  submitButtonText: {
+    color: "white"
+  }
+});
+
+const loginstyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 50
+  },
+  contentContainer: {
+    paddingTop: 40
+  },
+  titleContainer: {
+    alignItems: "center",
+    marginTop: 10,
+    marginHorizontal: 30,
+    flexDirection: "row"
+  },
+  backgroundContainer: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+    marginTop: 0,
+    marginLeft: -10
+  },
+  titleLetters: {
+    color: "rgba(96,100,109, 1)",
+    fontSize: 24,
+    lineHeight: 24,
+    alignSelf: "flex-start"
+  },
+  textLetters: {
+    color: "rgba(96,100,109, 1)",
+    fontSize: 16,
+    alignSelf: "flex-start",
+    margin: 15,
+    marginTop: 0,
+    marginBottom: 0,
+    marginHorizontal: 30,
+  },
+  pass: {
+    color: "rgba(96,100,109, 1)",
+    fontSize: 16,
+    alignSelf: "flex-start",
+    margin: 15,
+    marginTop: 15,
+    marginBottom: 0,
+    marginHorizontal: 30,
+  },
+
+  titleLettersblue: {
+    color: "#77A8AC",
+    fontSize: 20,
+    lineHeight: 24,
+    textAlign: "center"
+  },
+  titleLettersgreen: {
+    color: "#6F8D71",
+    fontSize: 20,
+    lineHeight: 24,
+    textAlign: "center"
+  }
+});
